6d4cca5c22f09be2533349265616e408348e5210
apoowa <98971@student.vitkovickastredni.cz>
2026-05-13 16:33:14 +0200

    commit_upravit

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Testovací data filmů
+movies = [
+    {
+        "id": 1,
+        "title": "The Handmaiden",
+        "image": "images/thehandmaiden.jpg"
+    },
+    {
+        "id": 2,
+        "title": "Bound",
+        "image": "images/bound.jpg"
+    },
+    {
+        "id": 3,
+        "title": "Redline",
+        "image": "images/redline.jpg"
+    }
+]
+
+
 @app.route("/")
 def home():
     return render_template("base.html")
