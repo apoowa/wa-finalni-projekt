53e0045332e7e5686f110270f03accc09344b8c3
apoowa <98971@student.vitkovickastredni.cz>
2026-05-14 17:59:17 +0200

    movies_details

--- app.py
+++ app.py
@@ -8,17 +8,26 @@ movies = [
     {
         "id": 1,
         "title": "The Handmaiden",
-        "image": "thehandmaiden.jpg"
+        "image": "thehandmaiden.jpg",
+        "description": "A woman is hired as a handmaid to a Japanese heiress, but secretly she is involved in a plot to defraud her.",
+        "cast": "Kim Min-hee, Kim Tae-ri",
+        "director": "Park Chan-wook"
     },
     {
         "id": 2,
         "title": "Bound",
-        "image": "bound.jpg"
+        "image": "bound.jpg",
+        "description": "A tough female ex-con and her lover hatch a scheme to steal millions of stashed mob cash.",
+        "cast": "Jennifer Tilly, Gina Gershon",
+        "director": "Lana & Lilly Wachowski"
     },
     {
         "id": 3,
         "title": "Redline",
-        "image": "redline.jpg"
+        "image": "redline.jpg",
+        "description": "A story about the most dangerous car race in the galaxy and the deadly illegal underworld that runs it.",
+        "cast": "Takuya Kimura, Yuu Aoi",
+        "director": "Takeshi Koike"
     }
 ]
 
